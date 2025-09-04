6d112d34f83a401853dc8662dd89d5077507dcab
frank-job <Jobf0837@gmail.com>
2025-09-04 07:17:33 -0700

    fixed images

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+// Dynamically set the current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+// Dynamically set last modified date and time
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
